62811ff4c41745e25d8cac7c19b53a8c4fbc2121
Chris Fowles <chris.fowles@vibrato.com.au>
2018-09-25 23:27:07 +1000

    Revert "Adds a new welcome screen to our site (#9)"
    This reverts commit c169e9439cd012c81649c64e075e50e1e04fec39.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Google Cloud logo" src="./assets/googlecloud.png">
-    <HelloWorld msg="Hello Google Cloud Summit 2018"/>
-    <div class="logos">
-      <img data-cy="logo-sm-cloudbuild" class="logo-small" alt="Google Cloud Build logo" src="./assets/cloudbuild.png">
-      <img data-cy="logo-sm-octocat" class="logo-small" alt="Octocat logo" src="./assets/octocat.png">
-      <img data-cy="logo-sm-appengine" class="logo-small" alt="Google App Engine logo" src="./assets/appengine.png">
-    </div>
-    <div>
-      <p class="reference-link">https://github.com/marketplace/google-cloud-build</p>
-      <p class="reference-link">https://github.com/chrisfowles/gcp-demo</p>
-      <p class="reference-link">https://www.cypress.io</p>
-      <p class="reference-link">https://vuejs.org</p>
-    </div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js Site"/>
   </div>
 </template>
 
@@ -35,13 +24,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.reference-link {
-  font-size: larger;
-}
-
-.logo-small {
-  height: 140px;
 }
 </style>

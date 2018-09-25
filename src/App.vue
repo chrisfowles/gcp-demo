715b4df6918a10b5f7febb74ef2305e6e5d7250a
Chris Fowles <chris.fowles@vibrato.com.au>
2018-09-25 21:41:11 +1000

    add some links

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,16 @@
   <div id="app">
     <img alt="Google Cloud logo" src="./assets/googlecloud.png">
     <HelloWorld msg="Hello Google Cloud Summit 2018"/>
-    <img class="logo-small" alt="Google Cloud Build logo" src="./assets/cloudbuild.png">
-    <img class="logo-small" alt="Octocat logo" src="./assets/octocat.png">
-    <img class="logo-small" alt="Google App Engine logo" src="./assets/appengine.png">
+    <div class="logos">
+      <img class="logo-small" alt="Google Cloud Build logo" src="./assets/cloudbuild.png">
+      <img class="logo-small" alt="Octocat logo" src="./assets/octocat.png">
+      <img class="logo-small" alt="Google App Engine logo" src="./assets/appengine.png">
+    </div>
+    <div>
+      <p>https://github.com/marketplace/google-cloud-build</p>
+      <p>https://github.com/chrisfowles/gcp-demo</p>
+      <p>https://www.cypress.io</p>
+    </div>
   </div>
 </template>
 
